85a64cc2cf34546817b9e98daa8293e1229ee6af
anegerega <angelaegerega@yahoo.com>
2021-03-14 18:37:17 +0100

    creating initial account entry points

--- Program.cs
+++ Program.cs
@@ -10,7 +10,28 @@ namespace BankSystem
         static void Main(string[] args)
         {
             Console.WriteLine("<<<<<<<<<<<<<<<< WELCOME TO PRESTIGE BANK >>>>>>>>>>>>>");
-            var newAccount = new BankAccount("Angela", 5000);
+
+            Console.WriteLine("How may we be of help to you today?");
+            Console.WriteLine("1. Open an account");
+            Console.WriteLine("2. Login to your account");
+            Console.WriteLine("3 .Exit");
+
+            string userchoice = Console.ReadLine();
+
+            switch (userchoice)
+            {
+                case "1":
+                    Console.WriteLine("");
+                    break;
+                case "2":
+                    Console.WriteLine("");
+                    break;
+                case "3":
+                    Console.WriteLine("");
+                    break;
+            }
+
+                    var newAccount = new BankAccount("Angela", 5000);
             Console.WriteLine($"An Account with number {newAccount.AccountNumber} has been successfully created for {newAccount.AccountOwner} with account balance of {newAccount.Balance}");
 
             newAccount.Withdraw(500, DateTime.Now, "Food");
